1681bddd9a89a14304f49d9e16824075fd83156c
liyuelin <2636063829@qq.com>
2018-09-17 23:33:11 +0800

    first commit

---
+++ src/pages/order.vue
@@ -0,0 +1,127 @@
+<!-- 论文检测订单管理 -->
+<template>
+    <div class="table">
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item><i class="el-icon-tickets"></i>论文检测订单列表</el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="container">
+            <div class="handle-box">
+              <!-- <el-select v-model="typeId" placeholder="请选择订单类型" @change="selectType">
+                  <el-option
+                    v-for="item in typeList"
+                    :key="item.id"
+                    :label="item.label"
+                    :value="item.id">
+                  </el-option>
+              </el-select> -->
+            </div>
+            <el-table :data="tableData" border style="width: 100%" ref="multipleTable">
+                <el-table-column prop="created_at" label="订单日期" sortable width="200"></el-table-column>
+                <el-table-column prop="name" label="用户名" width="120"></el-table-column>
+                <el-table-column prop="room_no" label="检测类型" width="120"></el-table-column>
+                <el-table-column prop="order_no" label="论文作者" width="200"></el-table-column>
+                <el-table-column prop="total" label="论文标题" width="120"></el-table-column>
+                <el-table-column prop="productName" label="论文价格" width="200"></el-table-column>
+                <el-table-column prop="productPrice" label="订单金额" width="200"></el-table-column>
+                <el-table-column prop="productPrice" label="订单编号" width="200"></el-table-column>
+                <el-table-column prop="remark" label="论文内容" width="200"></el-table-column>
+            </el-table>
+            <div class="pagination">
+                <el-pagination background @current-change="handleCurrentChange" :page-size="pageSize" layout="prev, pager, next" :total="total">
+                </el-pagination>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    import { apiOrderList } from '@/service'
+    export default {
+        data() {
+            return {
+                typeList: [{
+                    label: '美食订单',
+                    id: 1
+                },{
+                    label: '服务订单',
+                    id: 2
+                }],
+                typeId: 1,
+                tableData: [],
+                cur_page: 1,
+                pageSize: 10,
+                total: 0,
+                select_cate: '',
+                select_word: '',
+                is_search: false
+            }
+        },
+        created() {
+            this.getData();
+        },
+        computed: {
+
+        },
+        methods: {
+            // 分页导航
+            handleCurrentChange(val) {
+                this.cur_page = val;
+                this.getData();
+            },
+            getData() {
+                apiOrderList({
+                    type: this.typeId,
+                    page: this.cur_page
+                })
+                .then((res) => {
+                    console.log('res-order',res.data)
+                    this.tableData = res.data.list
+                    this.tableData.forEach(function(item){
+                        item.type = item.type == 1?'美食订单':'服务订单'
+                        item.productName = item.detail[0].name
+                        item.productPrice = item.detail[0].price
+                        item.productAmount = item.detail[0].amount
+                    })
+                    this.total = res.data.total
+                })
+            },
+            selectType(){
+              this.getData()
+            },
+            search() {
+                this.is_search = true;
+            },
+            // 保存编辑
+            saveEdit() {
+
+            },
+            // 确定删除
+            deleteRow(){
+
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+    .handle-box {
+        margin-bottom: 20px;
+    }
+
+    .handle-select {
+        width: 120px;
+    }
+
+    .handle-input {
+        width: 300px;
+        display: inline-block;
+    }
+    .del-dialog-cnt{
+        font-size: 16px;
+        text-align: center
+    }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,84 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+Vue.use(Router);
+
+export default new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            redirect: '/login'
+        },
+        {
+            path: '/',
+            component: resolve => require(['../components/common/Home.vue'], resolve),
+            meta: { title: '自述文件' },
+            children:[
+                {
+                    path: '/dashboard',
+                    component: resolve => require(['../pages/Dashboard.vue'], resolve),
+                    meta: { title: '系统首页' }
+                },
+                {
+                    path: '/order',
+                    component: resolve => require(['../pages/order.vue'], resolve),
+                    meta: { title: '论文检测订单' }
+                },
+                {
+                    path: '/fee',
+                    component: resolve => require(['../pages/fee.vue'], resolve),
+                    meta: { title: '降重收费' }
+                },
+                {
+                    path: '/editor',
+                    component: resolve => require(['../pages/editor.vue'], resolve),
+                    meta: { title: '酒店详情' }
+                },
+                {
+                    // 账号管理
+                    path: '/account',
+                    component: resolve => require(['../pages/account.vue'], resolve),
+                    meta: { title: '账号管理' }
+                },
+                {
+                    // 充值订单
+                    path: '/recharge',
+                    component: resolve => require(['../pages/recharge.vue'], resolve),
+                    meta: { title: '充值订单' }
+                },
+                {
+                    // 人工查重
+                    path: '/check',
+                    component: resolve => require(['../pages/check.vue'], resolve),
+                    meta: { title: '人工查重' }
+                },
+                {
+                    //检测类型
+                    path: '/type',
+                    component: resolve => require(['../pages/type.vue'], resolve),
+                    meta: { title: '检测类型' }
+                }
+            ]
+        },
+        {
+            path: '/login',
+            component: resolve => require(['../pages/Login.vue'], resolve),
+            meta: { title : '论文'}
+        },
+        {
+            path: '/404',
+            component: resolve => require(['../pages/404.vue'], resolve)
+        },
+        {
+            path: '/403',
+            component: resolve => require(['../pages/403.vue'], resolve),
+             meta: { title : '403'}
+        },
+        {
+            path: '*',
+            redirect: '/404',
+            meta: { title : '404'}
+        }
+    ]
+})
